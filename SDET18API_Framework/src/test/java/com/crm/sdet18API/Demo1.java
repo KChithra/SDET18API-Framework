0185b8414e898a95ad60407fd21b246cf202e6cf
KChithra <chithra.nethra05@gmail.com>
2021-08-04 13:41:39 +0530

    add API framework

---
+++ SDET18API_Framework/src/test/java/com/crm/sdet18API/Demo1.java
@@ -0,0 +1,12 @@
+package com.crm.sdet18API;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+@Test
+public void  demoTest() {
+	
+	System.out.println("Hi, there im in API framework");
+}
+	
+}
